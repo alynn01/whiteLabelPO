d4356e0e122673fbeda5a85d57da2e21c27735e4
Uju <ujuemaikeonu@gmail.com>
2023-04-17 16:18:26 +0100

    first commit

--- cypress/support/pages/index.js
+++ cypress/support/pages/index.js
@@ -2,4 +2,5 @@ export * from "./Program Management/createProgram-page";
 export * from "./User Management/createUser-page";
 export * from "./Wallet Management/fundProgram-page";
 export * from "./Wallet Management/fundUser-page";
-export * from "./shared/login-page"
+export * from "./shared/login-page";
+export * from "./Program Management/moreDetails-page"
