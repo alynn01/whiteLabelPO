74b2fdd779d0ca36f69c6d7d2d262c65dfb802b6
alynn01 <127193049+alynn01@users.noreply.github.com>
2023-04-26 19:46:49 +0100

    formattin with pretteir
    prettier formatting

--- cypress/e2e/Program Management/createProgram.cy.js
+++ cypress/e2e/Program Management/createProgram.cy.js
@@ -1,25 +1,23 @@
 import { getProgramOwner } from "../../fixtures/index.js";
 import { LoginPage, CreateProgramPage } from "../../support/pages/index";
 
-
 const loginPage = new LoginPage();
-const createProgram = new CreateProgramPage()
+const createProgram = new CreateProgramPage();
 const getPO = getProgramOwner();
 
-describe('Create Program', () => {
+describe("Create Program", () => {
+  beforeEach(() => {
+    loginPage.accessLoginModal();
+    loginPage.login(getPO.email, getPO.password);
+  });
 
-    beforeEach(() => {
-      loginPage.accessLoginModal()
-      loginPage.login(getPO.email, getPO.password);
-    });
+  it("Test that user is able to navigate to create program popup", () => {
+    createProgram.accessCreateProgram();
+    createProgram.enterProgramDetails();
+  });
 
-    it('Test that user is able to navigate to create program popup', () => {
-       createProgram.accessCreateProgram();
-       createProgram.enterProgramDetails();
-    })
-
-    it('Test that user is unable to submit the form if the details arent completed', () => {
-      createProgram.accessCreateProgram();
-      createProgram.clickSubmitWithoutData();
-   })
-})
+  it("Test that user is unable to submit the form if the details arent completed", () => {
+    createProgram.accessCreateProgram();
+    createProgram.clickSubmitWithoutData();
+  });
+});
--- cypress/support/pages/Wallet Management/fundProgram-page.js
+++ cypress/support/pages/Wallet Management/fundProgram-page.js
@@ -1,26 +1,28 @@
-export class FundProgramUserPage{
-    walletMenuItem = () => cy.get('[href="/dashboard/wallet"] > span');
-    quickActionDropdown = () => cy.get(`[class*="placeholder"]`).contains("Quick actions");
-    fundProgramDropdownItem = () => cy.get(`div[type="button"]`).contains('Fund programs')
-    programDropdown = () => cy.get(`input[placeholder="Program"]`);
-    selectProgram = () => cy.get('.sc-lllmON > :nth-child(3)').contains("Amas Program");
-    enterAmount = () => cy.get(`input[name="amount"]`);
-    fundProgramButton = () => cy.get('.button-text').contains('Fund program')
-    popupHeader = () => cy.get('.modal-header-content > span').contains('Fund program accounts')
+export class FundProgramUserPage {
+  walletMenuItem = () => cy.get('[href="/dashboard/wallet"] > span');
+  quickActionDropdown = () =>
+    cy.get(`[class*="placeholder"]`).contains("Quick actions");
+  fundProgramDropdownItem = () =>
+    cy.get(`div[type="button"]`).contains("Fund programs");
+  programDropdown = () => cy.get(`input[placeholder="Program"]`);
+  selectProgram = () =>
+    cy.get(".sc-lllmON > :nth-child(3)").contains("Amas Program");
+  enterAmount = () => cy.get(`input[name="amount"]`);
+  fundProgramButton = () => cy.get(".button-text").contains("Fund program");
+  popupHeader = () =>
+    cy.get(".modal-header-content > span").contains("Fund program accounts");
 
+  accessFundProgramPage() {
+    this.walletMenuItem().click();
+    this.quickActionDropdown().click({ force: true });
+    this.fundProgramDropdownItem().click();
+    this.popupHeader().should("be.visible");
+  }
 
-    accessFundProgramPage(){
-        this.walletMenuItem().click()
-        this.quickActionDropdown().click({force : true})
-        this.fundProgramDropdownItem().click()
-        this.popupHeader().should('be.visible')
-    }
-
-    enterFundDetails(fundingAmount){
-        this.programDropdown().click({force : true})
-        this.selectProgram().click({force : true})
-        this.enterAmount().type(fundingAmount)
-        this.fundProgramButton().should('be.visible')
-    }
-
+  enterFundDetails(fundingAmount) {
+    this.programDropdown().click({ force: true });
+    this.selectProgram().click({ force: true });
+    this.enterAmount().type(fundingAmount);
+    this.fundProgramButton().should("be.visible");
+  }
 }
