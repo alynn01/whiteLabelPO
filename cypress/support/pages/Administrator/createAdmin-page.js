236f095f488bd5bb2284d13d29171b69dc9f9c99
alynn01 <127193049+alynn01@users.noreply.github.com>
2023-04-27 04:29:40 +0100

    added admin module
    Added create admin
    change based to Safex

---
+++ cypress/support/pages/Administrator/createAdmin-page.js
@@ -0,0 +1,36 @@
+export class CreateAdminPage{
+
+    adminTab = () => cy.get('[href="/dashboard/administrators"]')
+    createNewAdminButton = () => cy.get(`[class="button-text"]`).contains("Create New Administrator ")
+    firstNameField = () => cy.get('input[name="firstName"]');
+    lastNameField = () => cy.get('input[name="lastName"]');
+    emailField = () => cy.get('input[name="emailAddress"]');
+    selectCountry = () => cy.get(`[class="selected-flag"]`);
+    searchCountryFiled = () => cy.get('input[placeholder="search"]');
+    phoneNumberFiled = () => cy.get('input[placeholder="Select Phone Number"]');
+    selectRole = () => cy.get(`[class="placeholder"]`).contains("Select role");
+    pickRole = () => cy.get('.sc-lllmON > :nth-child(3)');
+    createAdminButton = () => cy.get(`[class="button-text"]`).contains("Create administrator")
+
+
+    accessAdminPage(){
+        this.adminTab().click()
+    }
+
+    createAdminUser(){
+        this.createNewAdminButton().click()
+    }
+
+    completeEntryFields(emailValue){
+        this.firstNameField().type("Roberto");
+        this.lastNameField().type("Carlos");
+        this.emailField().type(emailValue);
+        this.selectCountry().click();
+        this.searchCountryFiled().type("Colombia");
+        cy.contains("Colombia").click()
+        this.phoneNumberFiled().type("5678578590")
+        this.selectRole().click()
+        this.pickRole().contains("Audit Admin").click({force : true})
+        this.createAdminButton().should("be.visible")
+    }
+}
